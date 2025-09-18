42bc9857285c30bb82d082132910f6407aa7f3fe
iburah <ibsailesi2@gmail.com>
2025-09-18 15:44:28 +0200

    User model file adding user.
    Created userModel file.
    The file handles all queries for creating user.
    INSERT operation has been used to insert username and password.
    An addUser module has been created ready for importing.

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+//This file willinteract directly with the database to manage uder data
+//SQL queries are handled here to create user and fetch user
+
+const pool = require('../utils/db'); //importng database pool connection
+
+//creating a new user in the database
+exports.addUser = async(username, passwordHash) =>{
+    //exports creates a reusable module: taking name of user and hashed password
+    const result = await pool.query(
+        `INSERT INTO users(username, password_hash)
+        VALUES ($1, $2)
+        RETURNING user_id, username`,
+        [username, passwordHash]
+    );
+
+    //inserting data into the columns named username and password_hash from the table named users
+    //$1 and $2 are 
+    //auto-generated primary keys (user_id) and username are returned in an array
+    //the query result will be stored in the result variable
+    
+    result.rows[0];
+
+    //newly created user is returned
+};
+
+//addUser module can be reused
