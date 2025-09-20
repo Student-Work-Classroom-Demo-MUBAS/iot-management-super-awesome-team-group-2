c710f8af42cf8bc80b8109c6bd30304addbe3be7
iburah <ibsailesi2@gmail.com>
2025-09-20 13:32:41 +0200

    Created basic Express app
    Added bodypaser middleware to parse req.body into JSON for userController to read from.
    Added CORS middleware to allow requests access to server frm different origins(domains, or ports).
    Connected userRoutes to express app.
    Added test route to check if backend is running.

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+//backend entry point
+const express = require('express');
+const bodyParser = require('body-parser');      //middleware for parsing JSON
+const cors = require('cors');           //middleware to handle cross-origin requests
+
+//import user routes from routes folder
+const userRoutes = require('../routes/userRoutes');
+
+//define express app
+const app = express();
+
+app.use(cors());        //enable CORS
+app.use(bodyParser.json);     //to parse incoming requests with JSON payloads  
+
+
+app.use('api/users', userRoutes);
+//mounting the user routes to /api/users, e.g. POST /api/users/register or POST /api/users/login
+
+//testing if API is running
+app.get('/', (req, res)=>{
+    res.send('IoT Backend API running ...');
+});
+
+//export app to use in server or in testing
+module.exports = app;
