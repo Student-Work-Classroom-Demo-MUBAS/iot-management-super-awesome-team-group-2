8ee9e542886315561b728d3a9fe980a93221c2da
iburah <ibsailesi2@gmail.com>
2025-09-20 14:34:21 +0200

    Updated the db.js
    Replaced hardcoded Database credentials with process.env variables.
    Added the .env file to securely store db credentials, JWT secret, and port.

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
+require('dotenv').config();   //it will load variables from .env
 
 const pool = new Pool({
-  user: 'postgres',          
-  host: 'localhost',
-  database: 'iot_project_db', 
-  password: 'sparkles02',  
-  port: 5432,
+  user: process.env.DB_USER,      //postgres       
+  host: process.env.DB_HOST,        //localhost
+  database: process.env.DB_DATABASE,             //iot_project_db 
+  password: process.env.DB_PASSWORD,    //sparkles02 
+  port: process.env.DB_PORT,            //5432
 });
+//credentials have been left here for collaborative development purposes, they will be removed at deployment
 
 module.exports = pool;
