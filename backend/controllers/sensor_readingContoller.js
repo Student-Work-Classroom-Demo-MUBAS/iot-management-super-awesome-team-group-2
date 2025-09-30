eefc3f0188632cf4c475f38c61484fbb721633b0
Takondwa02 <mphandetk02@gmail.com>
2025-09-30 21:59:41 +0200

    creating and deleting a reading by specific ID

--- backend/controllers/sensor_readingContoller.js
+++ backend/controllers/sensor_readingContoller.js
@@ -20,3 +20,27 @@ async function getReadingsByDeviceId(req, res) {
     res.status(500).json({ error: 'Failed to fetch sensor readings for device' });
   }
 }
+
+async function createReading(req, res) {
+  try {
+    const { device_id, temperature, humidity, soil_moisture, timestamp } = req.body;
+    const newReading = await sensorReadingModel.createReading(device_id, temperature, humidity, soil_moisture, timestamp);
+    res.status(201).json(newReading);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to create sensor reading' });
+  }
+}
+
+// Delete a sensor reading by ID
+async function deleteReading(req, res) {
+  try {
+    const readingId = parseInt(req.params.readingId);
+    const deletedReading = await sensorReadingModel.deleteReading(readingId);
+    if (!deletedReading) {
+      return res.status(404).json({ error: 'Sensor reading not found' });
+    }
+    res.status(200).json({ message: 'Sensor reading deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete sensor reading' });
+  }
+}
