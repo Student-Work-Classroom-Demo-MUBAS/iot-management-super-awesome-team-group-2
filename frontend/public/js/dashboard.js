80a6d28c473075d390dd2f9d9ce0fbe30a1a69de
Tatyana C <165555578+Nachya7@users.noreply.github.com>
2025-10-30 04:06:20 +0200

    Getting elements by id and assigning to defined variables

---
+++ frontend/public/js/dashboard.js
@@ -0,0 +1,8 @@
+// -------------------------------
+//  Dashboard Live Update Script
+// -------------------------------
+
+const tempEl = document.getElementById("temperature-value");
+const humidityEl = document.getElementById("humidity-value");
+const moistureEl = document.getElementById("moisture-value");
+const hourlyDataContainer = document.getElementById("hourly-data");
